173d51c8b3a530988fcea762057849032cda690f
ndq809 <ndq809@gmail.com>
2020-05-27 12:03:48 +0900

    tai lieu dac ta

---
+++ Docs/Create_DB/create_DB.sql
@@ -0,0 +1,11 @@
+﻿
+SELECT 
+	COLUMN_NAME
+,	DATA_TYPE
+,	IIF(DATA_TYPE IN ('smallint','int','tinyint','smallmoney','money','bigint'),N'Số',IIF(DATA_TYPE IN ('nvarchar','ntext'),N'Chữ',N'Ngày tháng'))
+,	IIF(CHARACTER_MAXIMUM_LENGTH IS NULL OR DATA_TYPE='ntext','',IIF(CHARACTER_MAXIMUM_LENGTH=-1,'Max',CONVERT(nvarchar(100),CHARACTER_MAXIMUM_LENGTH)))
+,	''
+,	''
+,	IIF(IS_NULLABLE='NO','x','')
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = N'F008'
